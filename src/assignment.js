521f294f44bea03580d55c3b10d894979ccd8e03
osasu <emovonosasuyi@gmail.com>
2022-07-15 04:50:33 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let total=0;
+for (
+    let i=0; i<arrayOfNumbers.length;i++)  {
+    total += arrayOfNumbers[i];
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+return total;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+      var count =0;
+ for (let index = 0; index < arrayOfNumbers.length; index++) {
+    if (arrayOfNumbers [index] % 2 ===0) {
+       count++;
+    }
+     }
+return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
